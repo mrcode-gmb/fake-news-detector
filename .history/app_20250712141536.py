ab08d8e761bdd2559453ca0e9e637235817052fe
Ahmad Isah <ahmadisah6293@gmail.com>
2025-07-16 21:35:22 +0100

    fake news detector

---
+++ .history/app_20250712141536.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+import joblib
+
+app = Flask(__name__)
+
+# Load the trained model and vectorizer
+model = joblib.load("model/model.pkl")
+vectorizer = joblib.load("model/vectorizer.pkl")
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/predict', methods=["POST"])
+
+def predict():
+    if request.method == "POST":
+        news = request.form['news']
+        data = vectorizer.transform([news])
+        prediction = model.predict(data)[0]
+
+        label = "Real News" if prediction == 1 else "Fake News"
+        return render_template("index.html", prediction=label)
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ .history/app_20250716212237.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request
+import joblib
+
+app = Flask(__name__)
+
+# Load the trained model and vectorizer
+model = joblib.load("model/model.pkl")
+vectorizer = joblib.load("model/vectorizer.pkl")
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+def clean_text(text):
+    import re, string
+    text = text.lower()
+    text = text.translate(str.maketrans('', '', string.punctuation))
+    text = re.sub(r'\d+', '', text)
+    text = re.sub(r'\s+', ' ', text).strip()
+    return text
+@app.route('/predict', methods=["POST"])
+
+
+def predict():
+    if request.method == "POST":
+        cleaned_news = clean_text(news)
+vectorized_news = vectorizer.transform([cleaned_news])
+
+prediction = model.predict(vectorized_news)[0]
+label = "Real News" if prediction == 1 else "Fake News"
+        return render_template("index.html", prediction=label)
+
+if __name__ == "__main__":
+    app.run(debug=True)
